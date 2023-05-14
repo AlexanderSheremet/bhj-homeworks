a21ce26e84afc4b16d7b3b78cfe88ebde5e21234
AlexanderSheremet <118627009+AlexanderSheremet@users.noreply.github.com>
2023-05-14 22:11:49 +0300

    Update task.js

--- js-features/countdown/task.js
+++ js-features/countdown/task.js
@@ -1,28 +1,45 @@
 //Задание №2
-const clock = document.getElementById("timer");
-
 const startTimer = setInterval(function () {
-  let time = stringTime;
-  if (time.numberTimer <= 0) {
+  let arrTimer = arrFunc();
+  let arrClock = arrTimer.arrClock;
+  let second = parseFloat(arrClock[2]);
+  let minute = parseFloat(arrClock[1]);
+  let hour = parseFloat(arrClock[0]);
+  let seconds = second % 60;
+  let minutes = Math.floor((minute / 60) % 60);
+  let hours = Math.floor((hour / 3600) % 24);
+  if (seconds != 0) {
+    seconds--;
+  }
+  if (minutes != 0) {
+    minutes--;
+  }
+  if (hours != 0) {
+    hours--;
+  }
+  if (hours <= 0 && minutes <= 0 && seconds <= 0) {
     alert("Вы победили в конкурсе!");
     clearInterval(startTimer);
   }
-  let outString = `${time.hours}:${time.minutes}:${time.seconds}`;
+  let outString = `${hours.toString().padStart(2, "0")}:${minutes
+    .toString()
+    .padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`;
+  clock = outString;
   timer.textContent = outString;
 }, 1000);
 
-const stringTime = function () {
-  let numberTimer = parseFloat(clock.textContent);
-  clock.textContent = numberTimer - 1;
-  let seconds = numberTimer % 60;
-  let minutes = Math.floor((numberTimer / 60) % 60);
-  let hours = Math.floor((numberTimer / 3600) % 24);
-  return {
-    numberTimer: numberTimer,
-    seconds: seconds,
-    minutes: minutes,
-    hours: hours,
-  };
+let clock = timer.textContent;
+const arrFunc = function () {
+  let arr = [clock, "0", "0"];
+  let second = parseFloat(arr[0]);
+  if (isNaN(second) == NaN || second === 0) {
+    let arr = [clock];
+    let arrClock = arr[0].split(":");
+    return { arrClock: arrClock };
+  } else {
+    arr = ["0", "0", clock];
+    return { arrClock: arr };
+  }
 };
 //Задание №1
 // const clock = document.getElementById("timer");

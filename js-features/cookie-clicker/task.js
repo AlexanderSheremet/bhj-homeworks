b7bcd7907140886873fc0dd6f08796d783a231fb
AlexanderSheremet <118627009+AlexanderSheremet@users.noreply.github.com>
2023-05-14 22:22:27 +0300

    Update task.js

--- js-features/cookie-clicker/task.js
+++ js-features/cookie-clicker/task.js
@@ -12,25 +12,30 @@
 //   }
 // };
 //Задание №2
-let clickerCounter = document.getElementById("clicker__counter");
-let clickerCookie = document.getElementById("cookie");
+const clickerStatus = document.querySelector(".clicker__status");
+clickerStatus.innerHTML += '<br>Скорость клика: <span id="click__speed">0</span>';
 
-clicker__counter.insertAdjacentHTML(
-  "afterEnd",
-  "<div class='clicker__status'>Скорость кликов:<span id='clicker__Speed'>0</span></div>"
-);
-const clikerSpeed = document.getElementById("clicker__Speed");
-const time = Date.now();
-clickerCookie.onclick = function () {
-  let counter = parseFloat(clickerCounter.textContent);
-  clickerCounter.textContent = counter + 1;
-  if (clickerCookie.width === 200) {
-    clickerCookie.width = 300;
-  } else {
-    clickerCookie.width = 200;
-  }
-  const timePast = Date.now();
-  clicker__Speed.textContent = (counter / ((timePast - time) / 1000)).toFixed(
-    2
-  );
-};
+const speedometer = document.getElementById('click__speed');
+const clicker = document.getElementById("clicker__counter");
+let clickCount = 0;
+let lastClickTime = null;
+
+cookie.onclick = function() {
+    const currentTime = new Date();
+    clickCount++;
+    clicker.innerHTML = clickCount;
+
+    if (clickCount % 2) {
+        cookie.width = "300";
+    } else {
+        cookie.width = "200";
+    }
+
+    if (lastClickTime) {
+        const timeDif = (currentTime - lastClickTime) / 1000;
+        const clickSpeed = 1 / timeDif;
+        speedometer.innerHTML = clickSpeed.toFixed(2);
+    }
+    
+    lastClickTime = currentTime;
+}

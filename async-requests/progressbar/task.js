4ad18b334e71c210a30a915058f1c2e58901ea8e
AlexanderSheremet <118627009+AlexanderSheremet@users.noreply.github.com>
2023-06-24 16:48:43 +0300

    Update task.js

--- async-requests/progressbar/task.js
+++ async-requests/progressbar/task.js
@@ -1,14 +1,15 @@
-const progress = document.getElementById('progress');
-const form = document.getElementById('form');
-
+const progress = document.getElementById("progress");
+const form = document.getElementById("form");
 const xhr = new XMLHttpRequest();
 
-form.addEventListener('change',event =>{
-  xhr.open('POST','https://students.netoservices.ru/nestjs-backend/upload');
-  xhr.upload.addEventListener('progress', event=>{
+form.addEventListener("submit", (event) => {
+  xhr.open("POST", "https://students.netoservices.ru/nestjs-backend/upload");
+  xhr.upload.addEventListener("progress", (event) => {
     progress.value = event.loaded / event.total;
-  })
+  });
+
   let formData = new FormData();
-xhr.send(formData);
-event.preventDefault();
-})
+  xhr.send(formData);
+
+  event.preventDefault();
+});
